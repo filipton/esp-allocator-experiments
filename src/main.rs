2738ba36ff70cc30763de9202f03f9ecc9a107c9
filipton <filipton12@gmail.com>
2024-09-20 13:07:05 +0200

    explore: serde with serde_json using esp-alloc

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 #![no_std]
 #![no_main]
 
+use alloc::{
+    string::{String, ToString},
+    vec,
+    vec::Vec,
+};
 use esp_backtrace as _;
 use esp_hal::{
     clock::ClockControl, delay::Delay, peripherals::Peripherals, prelude::*, system::SystemControl,
 };
-
+use serde::{Deserialize, Serialize};
 extern crate alloc;
 use core::mem::MaybeUninit;
 
@@ -32,8 +37,35 @@ fn main() -> ! {
 
     esp_println::logger::init_logger_from_env();
 
+    let test_ser = TestEnum::Test2(TestStruct {
+        names: vec![
+            "Filip".to_string(),
+            "cxzczx".to_string(),
+            "ewqewqgrhegfhejrwkhgkrfjewhkj".to_string(),
+        ],
+        id: 582948912849169420,
+    });
+    let res = serde_json::to_string(&test_ser).unwrap();
+    log::info!("Serialized: {:?}", res);
+
+    let test_deser: TestEnum = serde_json::from_str(&res).unwrap();
+    log::info!("Deserialized: {:?}", test_deser);
+
     loop {
         log::info!("Hello world!");
         delay.delay(500.millis());
     }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(tag = "type", content = "data")]
+enum TestEnum {
+    Test1 { wow: u128, no: String },
+    Test2(TestStruct),
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct TestStruct {
+    names: Vec<String>,
+    id: u128,
 }
