ce5539cedbd3ee154aab7aa9285e6eac5f8183d9
filipton <filipton12@gmail.com>
2024-09-20 16:04:23 +0200

    feat: own logger instance based on esp_println::logger::EspLogger

--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,7 @@ fn main() -> ! {
     let delay = Delay::new(&clocks);
     init_heap();
 
-    esp_println::logger::init_logger_from_env();
+    SimpleLogger::init_logger_from_env();
 
     let test_ser = TestEnum::Test2(TestStruct {
         names: vec![
@@ -68,4 +68,61 @@ enum TestEnum {
 struct TestStruct {
     names: Vec<String>,
     id: u128,
+}
+
+const LOG_TARGETS: Option<&'static str> = option_env!("ESP_LOGTARGETS");
+struct SimpleLogger;
+
+impl SimpleLogger {
+    pub fn init_logger_from_env() {
+        unsafe {
+            log::set_logger_racy(&Self).unwrap();
+        }
+
+        const LEVEL: Option<&'static str> = option_env!("ESP_LOGLEVEL");
+
+        if let Some(lvl) = LEVEL {
+            let level = <log::LevelFilter as core::str::FromStr>::from_str(lvl)
+                .unwrap_or_else(|_| log::LevelFilter::Off);
+            unsafe { log::set_max_level_racy(level) };
+        }
+    }
+}
+
+impl log::Log for SimpleLogger {
+    fn enabled(&self, _metadata: &log::Metadata) -> bool {
+        true
+    }
+
+    fn log(&self, record: &log::Record) {
+        // check enabled log targets if any
+        if let Some(targets) = LOG_TARGETS {
+            if targets
+                .split(",")
+                .find(|v| record.target().starts_with(v))
+                .is_none()
+            {
+                return;
+            }
+        }
+
+        const RESET: &str = "\u{001B}[0m";
+        const RED: &str = "\u{001B}[31m";
+        const GREEN: &str = "\u{001B}[32m";
+        const YELLOW: &str = "\u{001B}[33m";
+        const BLUE: &str = "\u{001B}[34m";
+        const CYAN: &str = "\u{001B}[35m";
+
+        let color = match record.level() {
+            log::Level::Error => RED,
+            log::Level::Warn => YELLOW,
+            log::Level::Info => GREEN,
+            log::Level::Debug => BLUE,
+            log::Level::Trace => CYAN,
+        };
+
+        esp_println::println!("{}{} - {}{}", color, record.level(), record.args(), RESET);
+    }
+
+    fn flush(&self) {}
 }
